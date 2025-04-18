2cec773da04fa306ca7f00dcaf88cee4314dfa31
Thuy Nguyen <thuthuynt1410@gmail.com>
2025-04-18 09:35:58 +0700

    3rd commit

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    serverActions: {
+      allowedOrigins: ['https://ohk-product.g.kuroco.app'],
+    },
+  },
 };
 
 export default nextConfig;
